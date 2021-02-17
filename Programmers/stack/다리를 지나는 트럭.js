d71f25a30b4c74a2113ec8b676f70d973e0bb0d3
Doo Lee <dlengjs12@naver.com>
2021-02-17 23:57:21 +0900

    알고리즘 문제풀이

---
+++ Programmers/stack/다리를 지나는 트럭.js
@@ -0,0 +1,29 @@
+let bridge_length = 2;
+let weight = 10;
+let truck_weights = [7,4,5,6];
+function solution(bridge_length, weight, truck_weights) {
+    let bridge = [];
+    let bridgeTime = [];
+    let time = 0;
+
+    for(let i=0;i<truck_weights.length;i++){
+        bridgeTime.push(0);
+    }
+
+    for(let i=0;i<truck_weights.length;){
+        bridge.push(truck_weights[i]);
+
+        if()
+    }
+
+
+    return time;
+}
+function sum(array){
+    let sum=0;
+    array.forEach(element => {
+        sum+=element
+    });
+    return sum;;
+}
+solution(bridge_length, weight, truck_weights);
