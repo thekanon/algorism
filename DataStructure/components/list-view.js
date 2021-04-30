120a7647eae0346ae3484622454adb93c98861ec
Doo Lee <dlengjs12@naver.com>
2021-04-30 23:32:47 +0900

    자료구조 1차 작업

--- DataStructure/components/list-view.js
+++ DataStructure/components/list-view.js
@@ -1,6 +1,6 @@
 customElements.define('list-view',
     class extends HTMLElement {
-        makeListView() {
+        makeList() {
             let str = "";
 
             this.array.map((el, i) => str += `
@@ -12,17 +12,14 @@ customElements.define('list-view',
 
             return str;
         }
-        constructor() {
-            super();
+        makeListView(){
+            if(this.shadowRootObj.querySelector(".array")){
+                this.shadowRootObj.removeChild(this.shadowRootObj.querySelector(".array"));
+                this.shadowRootObj.removeChild(this.shadowRootObj.querySelector("style"));    
+            }
+            const arrayData = this.makeList()
 
-
-            const template = document.createElement('template');
-
-            this.array = ["하나", "둘", "셋", "넷", "파이브", "six"];
-
-            const arrayData = this.makeListView()
-
-            template.innerHTML = `
+            this.template.innerHTML = `
                 <div class="array">
                     <h4>array name</h4>
                     <table>
@@ -30,17 +27,14 @@ customElements.define('list-view',
                             <col width="20%">
                             <col width="80%">
                         </colgroup>
-                        <thead>
+                        <tbody>
                             `+ arrayData + `
                         </tbody>
                     </table>        
                 </div>  
             `
 
-            const templateContent = template.content;
-
-            const shadowRoot = this.attachShadow({ mode: 'open' });
-
+            this.templateContent = this.template.content;
             const style = document.createElement('style');
             style.textContent = `
                 * { color:white }
@@ -48,11 +42,29 @@ customElements.define('list-view',
                 .array table { width:100%;text-align:center }
                 .array h4 { text-align:center }
                 td { border: 1px solid}
-            
             `;
 
-            shadowRoot.appendChild(style);
-            shadowRoot.appendChild(templateContent);
+            this.shadowRootObj.appendChild(style);
+            this.shadowRootObj.appendChild(this.templateContent);
+        }
+        pushArray(e,data){
+            this.array.push(data);
+            this.makeListView();
+            console.log(this.array)
+        }
+        popArray(){
+            this.array.pop();
+            this.makeListView();
+            console.log(this.array)
+        }
+        constructor() {
+            super();
+            this.shadowRootObj = this.attachShadow({ mode: 'open' });
+            this.array = ["하나", "둘", "셋", "넷", "파이브", "six"];
+
+            this.template = document.createElement('template');
+            this.templateContent = '';
+            this.makeListView();
         }
     }
 );
