60d5a7b1359d7aae7ece3fd23a87de268d65d935
Doo Lee <dlengjs12@naver.com>
2021-05-02 22:44:45 +0900

    리스트뷰 기능 추가

--- DataStructure/components/list-view.js
+++ DataStructure/components/list-view.js
@@ -57,8 +57,37 @@ customElements.define('list-view',
             this.makeListView();
             console.log(this.array)
         }
+        async insertArray(e, idx,data){
+            console.log(idx,data)
+            //data 이후 모든 값을 1칸씩 민다.
+            this.array.push('');
+            await this.sleep(500).then(() => this.makeListView())
+            let i=this.array.length-1
+            while(i>idx){
+                this.array[i]=this.array[i-1]
+                console.log(i)
+                this.makeListView()
+                await this.sleep(500).then(() => i-=1)
+            }
+            this.array[idx]=data;
+            this.makeListView()
+        }
+        renderView(){
+            setTimeout(() => {
+                console.log("11")
+                this.makeListView();
+            }, this.timeout);
+        }
+        deleteArray(e,data){
+            console.log(data)
+            this.makeListView();
+        }
+        sleep(ms) {
+            return new Promise((r) => setTimeout(r, ms))
+        }
         constructor() {
             super();
+            this.timeout = 1000;
             this.shadowRootObj = this.attachShadow({ mode: 'open' });
             this.array = ["하나", "둘", "셋", "넷", "파이브", "six"];
 
